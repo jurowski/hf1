ce2263a231f82ca3adf8b20a55749894c63905cc
Sandon Jurowski <jurowski@gmail.com>
2012-12-21 00:10:17 -0600

    updated discovery widget to write referer info

--- public/javascripts/widget_signup.js
+++ public/javascripts/widget_signup.js
@@ -5,16 +5,14 @@
   url += "?" + "host=" + current_location + "&key="+unique_key_each_host;
 
 
-  if (!/^http:\/\/([a-z0-9]+\.)?(habitforge\.com|discovery\.com)/.test(top.location.href)) {
-    alert('The HabitForge Widget is currently only available by invite only. Email support@habitforge.com for more details.');
-    return;
-  }
-  //if (!/^http:\/\/([a-z0-9]+\.)?habitforge\.com/.test(document.referrer)) {
-  //  alert('The HabitForge Widget is currently only available by invite only.');
+  //if (!/^http:\/\/([a-z0-9]+\.)?(habitforge\.com|discovery\.com)/.test(top.location.href)) {
+  //  alert('The HabitForge Widget is currently only available by invite only. Email support@habitforge.com for more details.');
   //  return;
   //}
 
   // sample url generated : http://www.quarkruby.com/show_data?host=quarkruby.com&width=200&key=quarkruby_123
+
+
   document.write("<div style='width:326px;height:209px;border-style:none;border:0px;background:url(http://www.habitforge.com/images/widget_bg_326x209.png);'>");
   document.write("<div style='margin-left:10px;'><img src='http://www.habitforge.com/images/habitforge_slogan.png' border=0 ></div>");
   document.write("<div style='margin-top:0px;padding-left:3px;padding-right:3px;padding-top:0px;'><iframe src='" + url + "' width='320' height='160' frameborder='0'></iframe></div>");
