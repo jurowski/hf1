d79ba31b4fbcc4a22da8394bdb4c7897a6718414
Sandon Jurowski <jurowski@gmail.com>
2013-05-09 06:12:46 -0500

    test change in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20130403185429) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+  
 
   create_table "affiliates", :force => true do |t|
     t.string   "email"
