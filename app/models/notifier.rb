9614f36baa53476e5c4afc7c38cda50f04c2852c
Sandon Jurowski <jurowski@gmail.com>
2013-05-15 05:12:32 -0500

    no longer send bcc to support for goal follower report weekly

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -394,7 +394,7 @@ class Notifier < ActionMailer::Base
   def weekly_report_of_goals_i_follow(user, arr_goals_to_email_me_about, day_of_the_week)
     recipients user.first_name + "<" + user.email + ">"
     #recipients "jurowski@gmail.com"
-    bcc        ["support@habitforge.com"]
+    #bcc        ["support@habitforge.com"]
     from       "HabitForge <support@habitforge.com>"
     subject    "[HF] #{day_of_the_week}'s Weekly Goal Follower Report"
     body       :user => user, :arr_goals_to_email_me_about => arr_goals_to_email_me_about
