bbb650e419a1e0dd11eca2bb5e85e27ac92d86a3
Sandon Jurowski <jurowski@gmail.com>
2013-07-15 06:17:16 -0500

    add migration for saving lagging success rates to db

---
+++ db/migrate/20130715104255_add_successrates_to_goals.rb
@@ -0,0 +1,26 @@
+class AddSuccessratesToGoals < ActiveRecord::Migration
+  def self.up
+  	add_column :goals, :success_rate_during_past_7_days, :integer
+  	add_column :goals, :success_rate_during_past_14_days, :integer
+  	add_column :goals, :success_rate_during_past_21_days, :integer
+  	add_column :goals, :success_rate_during_past_30_days, :integer
+  	add_column :goals, :success_rate_during_past_60_days, :integer
+  	add_column :goals, :success_rate_during_past_90_days, :integer
+  	add_column :goals, :success_rate_during_past_180_days, :integer
+  	add_column :goals, :success_rate_during_past_270_days, :integer
+  	add_column :goals, :success_rate_during_past_365_days, :integer
+  end
+
+  def self.down
+  	remove_column :goals, :success_rate_during_past_7_days
+  	remove_column :goals, :success_rate_during_past_14_days
+  	remove_column :goals, :success_rate_during_past_21_days
+  	remove_column :goals, :success_rate_during_past_30_days
+  	remove_column :goals, :success_rate_during_past_60_days
+  	remove_column :goals, :success_rate_during_past_90_days
+  	remove_column :goals, :success_rate_during_past_180_days
+  	remove_column :goals, :success_rate_during_past_270_days
+  	remove_column :goals, :success_rate_during_past_365_days
+  end
+
+end
