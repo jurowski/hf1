05418ea0f734d28316979dfad30cd35f2d417d4e
Sandon Jurowski <jurowski@gmail.com>
2013-08-14 17:53:09 -0500

    require SSL for login page but also for account edit page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -56,11 +56,13 @@ class ApplicationController < ActionController::Base
     def redirect_to_ssl_login
       #if request.url.include? 'http://' and (local_request? == false)
       ### don't want to force the sponsored sites to SSL cause they'll say "untrusted connection" due to cert name
-      if request.url.include? 'http://habitforge.com/user_session/new' and (local_request? == false)
+      if (request.url.include? 'http://habitforge.com/user_session/new' or request.url.include? 'http://habitforge.com/account/edit') and (local_request? == false)
           old_url = request.url
           new_url = request.url.sub("http://", "https://")
         redirect_to new_url
       end
+
+
     end 
 
     def server_root_url
