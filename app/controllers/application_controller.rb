755c367bd6b0750308870c20d103ef6b411e93b0
Sandon Jurowski <jurowski@gmail.com>
2013-08-22 05:47:05 -0500

    https vs http if https but not pw related page, redirect to http

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -61,7 +61,7 @@ class ApplicationController < ActionController::Base
         if (request.url.include? 'https://')
           ### landed on https
 
-          if (request.url.include? '/user_session' or request.url.include? '/account/edit')          
+          if (request.url.include? '/user_session/new' or request.url.include? '/user_session/destroy' or request.url.include? '/account/edit')          
             ### yes we should stay on https
             stay_here = true
           else
@@ -75,7 +75,7 @@ class ApplicationController < ActionController::Base
         else
           ### landed on http
 
-          if (request.url.include? '/user_session' or request.url.include? '/account/edit')          
+          if (request.url.include? '/user_session/new' or request.url.include? '/user_session/destroy' or request.url.include? '/account/edit')          
             ### hey, we should be on https
             stay_here = false
             old_url = request.url
