554723c938f88834f4621372c34d7a2e7f306b3c
Sandon Jurowski <jurowski@gmail.com>
2014-01-17 10:29:16 -0600

    implement asked_for_job_lead_on

--- app/send_job_search_request.rb
+++ app/send_job_search_request.rb
@@ -4,6 +4,18 @@ require 'logger'
 class SendJobSearchRequest < ActiveRecord::Base
   # This script emails people asking people if they know of any good job opportunities
 
+
+  ### RUN IN PRODUCTION:
+  ### cd /habitforge/current;RAILS_ENV=production /usr/bin/ruby /home/jurowsk1/etc/rails_apps/habitforge/current/script/runner /home/jurowsk1/etc/rails_apps/habitforge/current/app/send_job_search_request.rb
+  #RAILS_ENV=production 
+  #/usr/bin/ruby 
+  #/home/jurowsk1/etc/rails_apps/habitforge/current/script/runner 
+  #/home/jurowsk1/etc/rails_apps/habitforge/current/app/send_job_search_request.rb
+
+  ### RUN IN DEV:
+  ### rvm use 1.8.7;cd /home/sgj700/rails_apps/hf1/;ruby script/runner app/send_job_search_request.rb
+
+
   tnow = Time.now
   tnow_Y = tnow.strftime("%Y").to_i #year, 4 digits
   tnow_m = tnow.strftime("%m").to_i #month of the year
@@ -32,11 +44,11 @@ class SendJobSearchRequest < ActiveRecord::Base
 
   user_conditions = ""
   user_conditions = user_conditions + "sponsor = 'habitforge'"
-  user_conditions = user_conditions + " and email = 'jurowski@gmail.com'"
+  #user_conditions = user_conditions + " and email = 'jurowski@gmail.com'"
 
-  # user_conditions = user_conditions + " and asked_for_job_lead_on is null"
+  user_conditions = user_conditions + " and asked_for_job_lead_on is null"
   user_conditions = user_conditions + " and first_name != 'unknown'"
-  # user_conditions = user_conditions + " and update_number_active_goals > 0"
+  user_conditions = user_conditions + " and update_number_active_goals > 0"
 
 
     @users = User.find(:all, :conditions => user_conditions, :limit => max)
